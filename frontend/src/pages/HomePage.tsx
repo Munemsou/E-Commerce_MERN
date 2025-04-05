d6a20d47e7e2c8ac524162030b85dabef94695ad
Munem <munem.soufi@gmail.com>
2025-04-05 07:48:05 +0200

    Add loading indicator to HomePage and refactor product grid layout

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,16 +1,16 @@
 import ProductCard from "../components/ProductCard";
 import { styled } from "@mui/material/styles";
-import Grid from "@mui/material/Grid2";
+import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import { useEffect, useState } from "react";
 import { Product } from "../types/Product";
 import { BASE_URL, BASE_URL_PRODUCTION } from "../constants/baseUrl";
+import CircularWithValueLabel from "../components/CircularProgress";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: "#fff",
   ...theme.typography.body2,
-  // padding: theme.spacing(2),
   margin: theme.spacing(2),
   textAlign: "center",
   color: theme.palette.text.secondary,
@@ -21,6 +21,7 @@ const Item = styled(Paper)(({ theme }) => ({
 
 const HomePage = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   const url =
@@ -32,23 +33,53 @@ const HomePage = () => {
         const res = await fetch(`${url}/product`);
         const data = await res.json();
         setProducts(data);
+        setLoading(false);
       } catch {
         setError(true);
+        setLoading(false);
       }
     };
 
     fetchData();
-  }, []);
+  }, [url]);
+
+  if (loading) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+        }}
+      >
+        <CircularWithValueLabel />
+      </Box>
+    );
+  }
 
   if (error) {
-    return <Box>Something went wrong, please try again later!</Box>;
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+        }}
+      >
+        Something went wrong, please try again later!
+      </Box>
+    );
   }
 
   return (
     <Box sx={{ mt: 2 }}>
       <Grid container spacing={2}>
         {products.map((product) => (
-          <Grid size={{ md: 4, sm: 6 }}>
+          <Grid item xs={12} sm={6} md={4} key={product._id.toString()}>
             <Item>
               <ProductCard {...product} _id={product._id.toString()} />
             </Item>
@@ -58,4 +89,5 @@ const HomePage = () => {
     </Box>
   );
 };
+
 export default HomePage;
